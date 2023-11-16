526fba86e3adfe1ca05673b4bb487bc186b5283a
harindergarcha <garchah@oregonstate.edu>
2023-11-17 00:43:23 +0500

    Add TravelPlanner component

---
+++ travel-planner/src/components/TravelPlanner.js
@@ -0,0 +1,13 @@
+// travel-planner/src/components/TravelPlanner.js
+import React from 'react';
+
+const TravelPlanner = () => {
+  return (
+    <div>
+      <h1>Travel Planner Component</h1>
+      <p>This is a basic React component for travel planning.</p>
+    </div>
+  );
+};
+
+export default TravelPlanner;
