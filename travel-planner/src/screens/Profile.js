3b55604757ca4dbecbd300a6051b6ff722209e4a
ra2230 <ra387880@gmail.com>
2024-01-30 06:33:44 +0300

    Profile.js User's Login Logic Enhancements

--- travel-planner/src/screens/Profile.js
+++ travel-planner/src/screens/Profile.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { React, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Buttons from '../components/Buttons.js';
 
@@ -8,77 +8,58 @@ const Profile = () => {
     const [user, userinfo] = useState(0);
     
     const navigate = useNavigate();
-    const account = () => {
-        navigate('/account');
-    };
-    const UserExperiences = () => {
-        navigate('/UserExperiences');
-    };
-    const favorites = () => {
-        navigate('/favorites');
-    };
-    const ratings = () => {
-        navigate('/ratings');
-    };
-    const trips = () => {
-        navigate('/trips');
-    };
+    const account = () => { navigate('/account'); };
+    const UserExperiences = () => { navigate('/UserExperiences'); };
+    const favorites = () => { navigate('/favorites'); };
+    const ratings = () => { navigate('/ratings'); };
 
-    // login check
-    /*useEffect(() => {
-        fetch("http://localhost:5000/smthn")
-            .then(response => response.json())
-            .then(login => check(login))
-            .catch(error => console.error('Error fetching data:', error));
-    }, []);
-
-    // user info
-    useEffect(() => {
-        if (login === 1) {
-            fetch("http://localhost:5000/user-info")
-                .then(response => response.json())
-                .then(user => userinfo(user))
-                .catch(error => console.error('Error fetching data:', error));
-        }
-    }, []);*/
+    // login check & user data
+    const userdata = async() => {
+        try {
+        const theuserinfo = await fetch("http://localhost:5000/user-info/:65b57e2b37f5c24ce79c5b6e")
+        userinfo(theuserinfo)
+        console.log(user)
+        check(1)
+        } catch(error) { onsole.error('Error fetching data:', error) }
+    }
+    useEffect( () => { userdata() }, []);
 
     return (
         <div>
             <Buttons/>
-            
             <div className="Profile">
                 {login === 0 && (
                     <div className="loginblur"></div>
                 )}
+                {login === 1 && (
+                    <div>
+                        <div className="ProfileAccount">
+                            <button onClick={account} class="ProfileAccountButton">
+                            <p>user name!</p>
+                            </button>
+                        </div>
 
-                <div>
-                <div className="ProfileAccount">
-                    <button onClick={account} class="ProfileAccountButton">
-                        <p>user name!</p>
-                    </button>
-                </div>
-                </div>
-                <div className='ProfileContainer'>
-                    <div className="ProfileUserExperiences">
-                        <button onClick={UserExperiences} class="ProfileUserExperiencesButton">
-                            <p>circlescircles</p>
-                        </button>
-                    </div>
-                    <div className='ProfileContainer2'>
-                        <div className="ProfileFavorites">
-                            <button onClick={favorites} class="ProfileFavoritesButton">
-                                <p>circlescirclestoo</p>
-                            </button>
-                        </div>
-                        <div className="ProfileRatings">
-                            <button onClick={ratings} class="ProfileRatingsButton">
-                                <p>circlescirclestoo?</p>
-                            </button>
+                        <div className='ProfileContainer'>
+                            <div className="ProfileUserExperiences">
+                                <button onClick={UserExperiences} class="ProfileUserExperiencesButton">
+                                    <p>circlescircles</p>
+                                </button>
+                            </div>
+                            <div className='ProfileContainer2'>
+                                <div className="ProfileFavorites">
+                                    <button onClick={favorites} class="ProfileFavoritesButton">
+                                        <p>circlescirclestoo</p>
+                                    </button>
+                                </div>
+                                <div className="ProfileRatings">
+                                    <button onClick={ratings} class="ProfileRatingsButton">
+                                        <p>circlescirclestoo?</p>
+                                    </button>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div>
-                </div>
+                )}
             </div>
         </div>
     )
