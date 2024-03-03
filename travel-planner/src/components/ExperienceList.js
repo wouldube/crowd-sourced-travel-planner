e87bb4ab8b776626dc6ba7c86ae61b5f47f564f7
patchh <almondh@oregonstate.edu>
2024-03-03 18:23:12 -0500

    Updated experience grid, favorites page and user experience page.

--- travel-planner/src/components/ExperienceList.js
+++ travel-planner/src/components/ExperienceList.js
@@ -25,29 +25,24 @@ const ExperienceList = ({setExpId}) => {
         <Container>
             <strong>More Experiences to Explore...</strong>
             <Grid container spacing={3}>
-                {experiences.map((allexp) => (
+                {experiences.slice(-8).map((allexp) => (
                     <Grid item xs={6}>
                     <Card onClick={() => {goToExperience(allexp._id)}}>
                         <Grid container spacing={1}>
                             <Grid item xs={12}>
-                                <img src={allexp.image} alt=" "></img>
+                                <img src={allexp.images} style={{maxWidth: "100%" }}/>
                             </Grid>
-                            <Grid item xs={12}>
+                            <Grid item xs={16}>
                                 <strong>{allexp.title}</strong>
                             </Grid>
                             <Grid item xs={12}>
-                                {allexp.location.coordinates[0]}, {allexp.location.coordinates[1]}
+                                <strong>Location: </strong>{allexp.location.coordinates[0]}, {allexp.location.coordinates[1]}
                             </Grid>
                             <Grid item xs={12}>
-                                Posted By: {allexp.owner}
+                                <strong>Posted By: </strong>{allexp.owner}
                             </Grid>
                             <Grid item xs={12}>
-                                {/* {allexp.reviews} */}
-                            </Grid>
-                            <Grid item xs={12}>
-                                <div className="ratingImage">
-                                    <img src="https://media.istockphoto.com/id/1306258842/photo/5-or-five-stars-sign-symbol-on-white-background-illustration-ranking-quality-service-review.jpg?s=612x612&w=0&k=20&c=PLhPtCoPZSUM9FSg9CAmTC_7b4WoHMYdaDHas64kg6M=" alt=" " />
-                                </div>
+                                <strong>Rating: </strong>{allexp.reviews}
                             </Grid>
                             <Grid item xs={12}>
                                 {allexp.description}
