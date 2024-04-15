af9c40a268af75c6a4a5d2739e27a4374cdf6170
patchh <almondh@oregonstate.edu>
2024-04-15 14:44:46 -0400

    Implemented Create Trip to User Exp, updated UI for trips pages, fixed delete trip button, updated button on exp grid.

--- travel-planner/src/components/Trip.js
+++ travel-planner/src/components/Trip.js
@@ -43,7 +43,7 @@ const Trip = ({ tripObject }) => {
                     </Button>
                 </Tooltip>
                 <Tooltip title="Delete Trip" followCursor>
-                    <Button onClick={() => { navigate(`update-trip`) }}>
+                    <Button onClick={() => deleteTrip(tripObject)}>
                         <DeleteForeverIcon />
                     </Button>
                 </Tooltip>
@@ -52,10 +52,16 @@ const Trip = ({ tripObject }) => {
                         <>
                             {(exp) && (
                                 <Grid item key={index} xs={4}>
+                                    {/* <Card variant="experience" style={{ */}
+                                        {/* backgroundImage: `radial-gradient(rgba(255, 255, 255, 0.3), rgba(117, 207, 235, 0.7)), url(${exp.images[0]})`}}>
+                                        <Container></Container> */}
+                                        {/* <h2 style={{ display:"block", width:"100%", height:"200px" }}>{exp.title}</h2> */}
+                                    {/* </Card> */}
                                     <Card variant="experience" style={{
-                                        backgroundImage: `radial-gradient(rgba(255, 255, 255, 0.3), rgba(117, 207, 235, 0.7)), url(${exp.images[0]})`
-                                    }}>
-                                        <Container><h3>{exp.title}</h3></Container>
+                                        backgroundImage: `radial-gradient(rgba(255, 255, 255, 0.3), rgba(117, 207, 235, 0.7)), url(${exp.images[0]})`}}>
+                                        <CardContent style={{ height: "150vh", transform: "scale(0.9)" }}>
+                                            <h3 style={{ display:"block", width:"100%", height:"100px" }}>{exp.title}</h3>
+                                        </CardContent>
                                     </Card>
                                 </Grid>
                             )}
